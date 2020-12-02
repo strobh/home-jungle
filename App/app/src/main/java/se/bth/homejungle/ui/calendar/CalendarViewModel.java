983dbb8d69dc858c4da1372567a5f0105a48805e
Tobias Heckel <developer@tobias-heckel.de>
2020-12-02 17:25:55 +0100

    click on checkbox sets plant to be watered today

--- App/app/src/main/java/se/bth/homejungle/ui/calendar/CalendarViewModel.java
+++ App/app/src/main/java/se/bth/homejungle/ui/calendar/CalendarViewModel.java
@@ -10,15 +10,18 @@ import java.util.List;
 import se.bth.homejungle.storage.entity.PlantWithSpecies;
 import se.bth.homejungle.storage.entity.view.CalendarEvent;
 import se.bth.homejungle.storage.repository.CalendarRepository;
+import se.bth.homejungle.storage.repository.PlantRepository;
 
 public class CalendarViewModel extends AndroidViewModel {
 
     private final CalendarRepository calendarRepository;
+    private final PlantRepository plantRepository;
     private final LiveData<List<CalendarEvent>> calendarEvents;
 
     public CalendarViewModel(Application application) {
         super(application);
         calendarRepository = new CalendarRepository(application);
+        plantRepository = new PlantRepository(application);
         calendarEvents = calendarRepository.getCalendarEvents();
     }
 
@@ -27,8 +30,8 @@ public class CalendarViewModel extends AndroidViewModel {
     }
 
 
-    public void waterPlant(long plantId){
-        //TODO: water plant in database and update list
+    public void waterPlant(long plantId) {
+        plantRepository.setLastWateredOfPlantToToday(plantId);
     }
 }
 
