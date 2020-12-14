e0c592e91ee345e302aa7447afdb6f6509edead0
Tobias Heckel <developer@tobias-heckel.de>
2020-12-15 00:54:54 +0100

    added formula to calculate distance

--- App/app/src/main/java/se/bth/homejungle/ui/marketplace/marketplace/MarketplaceFragment.java
+++ App/app/src/main/java/se/bth/homejungle/ui/marketplace/marketplace/MarketplaceFragment.java
@@ -90,6 +90,7 @@ public class MarketplaceFragment extends LocationFragment implements LocationFra
         recyclerView.setAdapter(adapter);
 
         /*SharedPreferences sp = getActivity().getSharedPreferences("userdata", MODE_PRIVATE);
+        distance(location.getLatitude(), location.getLongitude(), LAT_OF_GIVEAWAY, LON_OF_GIVEAWAY);
         if(sp.contains("userid")){
             Log.v("MarketplaceFragment", "has userid: " + sp.getString("userid", null));
             marketplaceViewModel.getOtherGiveawaysLiveData(sp.getString("userid", null)).observe(getViewLifecycleOwner(), Observable -> {});
@@ -105,5 +106,21 @@ public class MarketplaceFragment extends LocationFragment implements LocationFra
                 adapter.submitList(marketplacePlants);
             });
         }*/
+    }
+
+    public static double distance(double lat1, double lon1, double lat2, double lon2) {
+        final int R = 6371; // Radius of the earth
+
+        double latDistance = Math.toRadians(lat2 - lat1);
+        double lonDistance = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = R * c * 1000; // convert to meters
+
+        distance = Math.pow(distance, 2);
+
+        return Math.sqrt(distance);
     }
 }
