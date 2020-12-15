a8929562e22d72b3e5117e9afc1440b84e8348d6
cvazouras <43879300+cvazouras@users.noreply.github.com>
2020-12-15 14:56:32 +0100

    plant info

--- App/app/src/main/java/se/bth/homejungle/ui/marketplace/single_giveaway/SingleGiveaway.java
+++ App/app/src/main/java/se/bth/homejungle/ui/marketplace/single_giveaway/SingleGiveaway.java
@@ -3,6 +3,7 @@ package se.bth.homejungle.ui.marketplace.single_giveaway;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -11,6 +12,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -21,7 +30,12 @@ import se.bth.homejungle.ui.MarketplacePlant;
 import se.bth.homejungle.ui.marketplace.marketplace.MarketplaceFragment;
 import se.bth.homejungle.ui.marketplace.marketplace.MarketplaceViewModel;
 
-public class SingleGiveaway extends Fragment {
+public class SingleGiveaway extends Fragment implements OnMapReadyCallback
+{
+    GoogleMap mGoogleMap;
+    MapView mMapView;
+    View mView;
+
     MarketplaceViewModel marketplaceViewModel;
     MarketplacePlant currentPlant;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -37,12 +51,13 @@ public class SingleGiveaway extends Fragment {
         // Inflate the layout for this fragment
         marketplaceViewModel = new ViewModelProvider(requireActivity()).get(MarketplaceViewModel.class);
         currentPlant = marketplaceViewModel.getCurrentPlant();
-        View root = inflater.inflate(R.layout.fragment_single_giveaway, container, false);
+        mView = inflater.inflate(R.layout.fragment_single_giveaway, container, false);
 
-        speciesName = root.findViewById(R.id.species_name);
-        userName = root.findViewById(R.id.tv_username);
-        distance = root.findViewById(R.id.tv_distance);
-        contact = root.findViewById(R.id.tv_contact);
+
+        speciesName = mView.findViewById(R.id.species_name);
+        userName = mView.findViewById(R.id.tv_username);
+        distance = mView.findViewById(R.id.tv_distance);
+        contact = mView.findViewById(R.id.tv_contact);
 
         speciesName.setText(currentPlant.getSpeciesname());
         userName.setText(currentPlant.getUsername());
@@ -50,7 +65,39 @@ public class SingleGiveaway extends Fragment {
         contact.setText(currentPlant.getContact());
 
 
-        return root;
+        return mView;
+    }
+
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+
+        mMapView = (MapView) mView.findViewById(R.id.mapView);
+        if (mMapView != null)
+        {
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+
+        }
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap)
+    {
+        MapsInitializer.initialize(getContext());
+
+        mGoogleMap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(56.160820, 15.586710)).title("Karlskrona"));
+
+        CameraPosition Karlskrona = CameraPosition.builder().target(new LatLng(56.160820, 15.586710)).zoom(16).bearing(0).tilt(45).build();
+
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(Karlskrona));
     }
 }
 
