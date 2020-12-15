ffc5e39befd5c246342e074164cf1d58090b6097
AnnCharBaer <48607023+AnnCharBaer@users.noreply.github.com>
2020-12-15 09:19:02 +0100

    database to plantpage

--- App/app/src/main/java/se/bth/homejungle/ui/plants/singleplant/PlantInfoFragment.java
+++ App/app/src/main/java/se/bth/homejungle/ui/plants/singleplant/PlantInfoFragment.java
@@ -3,10 +3,13 @@ package se.bth.homejungle.ui.plants.singleplant;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,13 +17,16 @@ import se.bth.homejungle.R;
 
 public class PlantInfoFragment extends Fragment
 {
-    long plantId;
+    long speciesId;
     TextView title;
     ImageView plantImg;
     TextView information;
+    ImageButton delete;
 
-    public PlantInfoFragment(long plantId){
-        this.plantId = plantId;
+    SinglePlantViewModel singlePlantViewModel;
+
+    public PlantInfoFragment(long speciesId){
+        this.speciesId = speciesId;
     }
 
     @Override
@@ -28,7 +34,27 @@ public class PlantInfoFragment extends Fragment
                              Bundle savedInstanceState)
     {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_plant_start2, container, false);
+        singlePlantViewModel = new ViewModelProvider(requireActivity()).get(SinglePlantViewModel.class);
 
+        View root = inflater.inflate(R.layout.fragment_plant_info, container, false);
+        title = root.findViewById(R.id.plantName);
+        plantImg = root.findViewById(R.id.plantPicture);
+        delete = root.findViewById(R.id.deleteButton);
+        singlePlantViewModel.getSpeciesById(speciesId).observe(getViewLifecycleOwner(), species -> {
+            title.setText(species.getName());
+        });
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deletePlant();
+            }
+        });
+
+        return root;
+
+    }
+
+    private void deletePlant() {
     }
 }
