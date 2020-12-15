13c79ff4df9c6d433b15e76c9a5a6ea4f7f51165
Tobias Heckel <developer@tobias-heckel.de>
2020-12-15 12:39:41 +0100

    changed category images and added relative date to your plants list

--- App/app/src/main/java/se/bth/homejungle/ui/plants/yourplants/YourPlantsListItem.java
+++ App/app/src/main/java/se/bth/homejungle/ui/plants/yourplants/YourPlantsListItem.java
@@ -1,5 +1,6 @@
 package se.bth.homejungle.ui.plants.yourplants;
 
+import android.annotation.SuppressLint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +11,8 @@ import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 import se.bth.homejungle.R;
 import se.bth.homejungle.storage.AppDatabase;
@@ -30,17 +32,31 @@ public class YourPlantsListItem extends RecyclerView.ViewHolder implements View.
         super(itemView);
         plant_name = itemView.findViewById(R.id.species_name);
         plant_desc = itemView.findViewById(R.id.user_name);
-        water_amount = itemView.findViewById(R.id.distance);
-        water_time = itemView.findViewById(R.id.tv_water_time);
+        water_amount = itemView.findViewById(R.id.water_amount);
+        water_time = itemView.findViewById(R.id.water_time);
         plant_img = itemView.findViewById(R.id.giveaway_img);
         itemView.setOnClickListener(this);
     }
 
+    @SuppressLint("SetTextI18n")
     public void bind(PlantWithSpecies plantWithSpecies) {
         plant_name.setText(plantWithSpecies.getSpecies().getName());
         plant_desc.setText(plantWithSpecies.getPlant().getDescription());
-        water_amount.setText(String.valueOf(plantWithSpecies.getSpecies().getWater()));
-        water_time.setText(plantWithSpecies.getNextWateringDate().format(DateTimeFormatter.ISO_DATE));
+        water_amount.setText((plantWithSpecies.getSpecies().getWater() * 0.2) + " l");
+
+        long daysBetween = ChronoUnit.DAYS.between(LocalDate.now(), plantWithSpecies.getNextWateringDate());
+        if (daysBetween < -1) {
+            water_time.setText(Math.abs(daysBetween) + " days ago");
+        } else if (daysBetween == -1) {
+            water_time.setText("yesterday");
+        } else if (daysBetween == 0) {
+            water_time.setText("today");
+        } else if (daysBetween == 1) {
+            water_time.setText("tomorrow");
+        } else {
+            water_time.setText("in " + daysBetween + " days");
+        }
+
         species_id = plantWithSpecies.getSpecies().getId();
         plant_img.setImageURI(AppDatabase.getUriForFileName(plantWithSpecies.getSpecies().getImage()));
     }
